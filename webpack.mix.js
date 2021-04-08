ee8518f1dbf1b8b2225be5b4479a720ac6e75bcb
poopedub <pablo.volt@gmail.com>
2021-04-08 17:53:29 +0200

    configuration of webpackmix

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,7 @@ require('@tinypixelco/laravel-mix-wp-blocks');
 
 mix
   .setPublicPath('./public')
-  .browserSync('sage.test');
+  .browserSync('localhost:10018');
 
 mix
   .sass('resources/styles/app.scss', 'styles')
